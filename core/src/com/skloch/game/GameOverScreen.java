d34706dea6178dc10abedcec13734ac96a92a6d0
jc3865 <jc3865@york.ac.uk>
2024-05-03 03:19:10 +0100

    leaderboard now stores the correct score.

--- core/src/com/skloch/game/GameOverScreen.java
+++ core/src/com/skloch/game/GameOverScreen.java
@@ -111,7 +111,7 @@ public class GameOverScreen implements Screen {
         // Currently does not calculate score but will provide 500 for the score.
         // NEEDS UPDATING
         List<String[]> newLeaderboardList = new ArrayList<>(Arrays.asList(game.leaderboard));
-        String[] playerArrayToAdd = new String[]{game.gameScreen.player.name, String.valueOf((new Random()).nextInt() % 500)};
+        String[] playerArrayToAdd = new String[]{game.gameScreen.player.name, String.valueOf(score)};
         newLeaderboardList.add(playerArrayToAdd);
 
         String[][] newLeaderboardArray = newLeaderboardList.toArray(new String[0][]);
