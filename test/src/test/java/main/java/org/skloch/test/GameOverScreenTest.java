ae3a6099a6aa756fb532b613855ff1ed6a477454
langelidis <la1152@york.ac.uk>
2024-05-20 03:32:00 +0100

    Updated GameOverScreenTest

--- test/src/test/java/main/java/org/skloch/test/GameOverScreenTest.java
+++ test/src/test/java/main/java/org/skloch/test/GameOverScreenTest.java
@@ -1,5 +1,6 @@
 package main.java.org.skloch.test;
 
+import main.java.org.skloch.game.GameOverScreen;
 import main.java.org.skloch.game.GameScreen;
 import main.java.org.skloch.game.HustleGame;
 import org.junit.Before;
@@ -7,25 +8,31 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
 
 @RunWith(GdxTestRunner.class)
 
 public class GameOverScreenTest {
-    GameScreen gameScreen;
-    HustleGame game;
+    private GameScreen gameScreen;
+    private GameOverScreen gameOverScreen;
+    private HustleGame game;
 
     @Before
     public void setUp(){
         game = new HustleGame(1280, 720);
         game.unitTest = true;
         gameScreen = new GameScreen(game, 1, "name");
+        gameOverScreen = mock(GameOverScreen.class);
     }
 
     @Test
-    public void gameOverScreenAfterSevenDays(){
+    public void gameOverScreenAfterSevenDays() {
         gameScreen.setDay(8);
         gameScreen.passTime(1);
         assertTrue(gameScreen.testGameOver);
     }
 
+    @Test
+    public void testRender() { gameOverScreen.render(1.0f);
+    }
 }
