6d716ce8450fab7d743194076575fb1d1e1b5d80
jc3865 <jc3865@york.ac.uk>
2024-05-23 01:53:06 +0100

    put the istreeeventcalled

--- core/src/main/java/org/skloch/game/EventManager.java
+++ core/src/main/java/org/skloch/game/EventManager.java
@@ -16,6 +16,8 @@ public class EventManager {
     private final HashMap<String, String> objectInteractions;
     private final Array<String> talkTopics;
 
+    private boolean isTreeEventCalled = false;
+
     /**
      * A class that maps Object's event strings to actual Java functions.
      * To run a function call event(eventString), to add arguments add dashes.
@@ -151,6 +153,11 @@ public class EventManager {
     public void treeEvent() {
         game.dialogueBox.hideSelectBox();
         game.dialogueBox.setText("The tree doesn't say anything back.");
+        isTreeEventCalled = true;
+    }
+
+    public boolean isTreeEventCalled() {
+        return isTreeEventCalled;
     }
 
     public void chestEvent() {
